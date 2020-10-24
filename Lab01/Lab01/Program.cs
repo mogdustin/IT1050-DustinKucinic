5dd1f2882b627c9a1a4c789dfc656dde10abf897
DKucinic <DKucinic27@gmail.com>
2020-10-24 13:14:57 -0400

    Created Lab 1

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Welcome1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Dustin Kucinic");
+        }
+    }
+}
